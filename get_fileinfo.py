d4fa4effd5de3b802d9ee7834235c780013f5744
mso <madhuojha34@gmail.com>
2024-03-02 23:12:05 +0545

    getting file info

---
+++ get_fileinfo.py
@@ -0,0 +1,19 @@
+import os
+
+
+def get_file_info(filename):
+    info = os.stat(filename)
+    return {
+        'Size': info.st_size,
+        'Permissions': oct(info.st_mode & 0o777),
+        'Last Accessed': info.st_atime,
+        'Last Modified': info.st_mtime,
+    }
+
+
+# Example usage:
+file_to_check = input("Enter path of file:")
+file_info = get_file_info(file_to_check)
+print("File Information:")
+for key, value in file_info.items():
+    print(f"{key}: {value}")
