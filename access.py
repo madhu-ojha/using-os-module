85d4ecf0e260588f031ce7aeaae8b4e871538e75
mso <madhuojha34@gmail.com>
2024-03-05 23:27:35 +0545

    comment update

--- access.py
+++ access.py
@@ -1,3 +1,5 @@
+# Using access method of os-module
+# os.access() is a method in Python's os module used to check the accessibility of a file path in the current process.
 import os
 import sys
 
@@ -5,7 +7,7 @@ fileName = "add.py"
 
 # Checks whether the file exists or not
 path1 = os.access(fileName, os.F_OK)
-print("Exist path: ", path1)     
+print("Exist path: ", path1)
 
 # Checks whether the file can be read or not
 path2 = os.access(fileName, os.R_OK)
@@ -18,4 +20,3 @@ print("Access to write the file: ", path3)
 # Checks whether the file has the permission to execute
 path4 = os.access(fileName, os.X_OK)
 print("Access to execute the file: ", path4)
- 
--- get_fileinfo.py
+++ get_fileinfo.py
@@ -1,3 +1,4 @@
+# Program to get metadata of files
 import os
 
 
